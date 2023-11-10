3813e4579965dd3deb2beae93967606e854cecde
Michael Brayer <michael@brayer.dev>
2023-11-10 14:35:15 +0100

    adds super basic plowing service

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "fcd_community.users",
     "fcd_community.billing",
+    "fcd_community.plowing",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
